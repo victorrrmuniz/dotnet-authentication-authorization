0634a0c1c3e873de9e55a0d63290b71d84419e3f
Victor Muniz <victorrmunizz@gmail.com>
2025-01-29 14:40:03 -0300

    create structure

---
+++ Shop/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Models;
+
+namespace Shop.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string userName, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, UserName = "batman", Password = "123", Role = "manager" });
+            users.Add(new User { Id = 2, UserName = "robin", Password = "321", Role = "employee" });
+            return users.Where(x => x.UserName.ToLower() == userName.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
---
+++ Shop/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    }
+}
